d054fc6e3ae86821f26dbe57d64a58a1a4553b54
Menard <menardsabugo@gmail.com>
2024-04-20 20:11:59 +0800

    Revert "Writing the pseudocode for the program"
    This reverts commit a87f40f4b4e7ba261fc0d3b9802cca4300e70858.

--- number_even_odd.py
+++
@@ -1,5 +0,0 @@
-# Reading the integers from numbers.txt file
-# Separating integers into even and odd number lists
-# Writing the even number list to "even.txt"
-# Writing the odd number list to "odd.txt"
-# Displaying the completion of the program
