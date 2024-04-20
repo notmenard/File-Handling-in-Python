db7f5a022421a9d146b53b318441fbcbf1846fe2
Menard <menardsabugo@gmail.com>
2024-04-20 20:26:06 +0800

    Writing the commands for program to read the integers

--- number_even_odd.py
+++ number_even_odd.py
@@ -1,4 +1,6 @@
 # Reading the integers from "number.txt" file
+with open("numbers.txt", "r") as new_file:
+    numbers = new_file.read().split()
 # Separating the integers into even and odd number lists
 # Writing the even number list to "even.txt"
 # Writing the odd number list to "odd.txt"
