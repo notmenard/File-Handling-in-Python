3777cb486b5052f1a89907cfe7e49388ac5efda2
Menard <menardsabugo@gmail.com>
2024-04-20 20:17:10 +0800

    Writing the pseudocode

---
+++ number_even_odd.py
@@ -0,0 +1,5 @@
+# Reading the integers from "number.txt" file
+# Separating the integers into even and odd number lists
+# Writing the even number list to "even.txt"
+# Writing the odd number list to "odd.txt"
+# Displaying that the program is executed
