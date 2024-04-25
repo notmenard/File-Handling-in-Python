cc02ed83f73ed3b04e1f1185adf4b30f061d4384
Menard <menardsabugo@gmail.com>
2024-04-26 01:35:26 +0800

    Pseudocode

--- text_writer_in_file.py
+++ text_writer_in_file.py
@@ -3,6 +3,6 @@ with open(mylife.txt, "a") as my_file:
 
     # Setting up a variable to hold the user's response.
     more_variables = "y"
-# Loop until the user confirms that there are no more lines
+    # Loop until the user confirms that there are no more lines
 # Collecting the user's input
 # Displaying the program's output
