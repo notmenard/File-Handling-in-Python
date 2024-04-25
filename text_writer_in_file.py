79b89d97a959d7f9f103ef2cc811d2f7034c8e5c
Menard <menardsabugo@gmail.com>
2024-04-26 01:29:46 +0800

    Opening the file in append mode

--- text_writer_in_file.py
+++ text_writer_in_file.py
@@ -1,4 +1,6 @@
 # Open the file in append mode
+with open(mylife.txt, "a") as my_file:
+
 # Setting up a variable to hold the user's response.
 # Loop until the user confirms that there are no more lines
 # Collecting the user's input
