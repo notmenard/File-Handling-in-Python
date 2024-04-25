1d9b6cf7947707811e1d7a2a08a201fa5a43b560
Menard <menardsabugo@gmail.com>
2024-04-25 17:03:21 +0800

    Writing the even number list

--- number_even_odd.py
+++ number_even_odd.py
@@ -3,11 +3,15 @@ with open("numbers.txt", "r") as new_file:
     numbers = new_file.read().split()
 
 # Separating the integers into even and odd number lists
-
 numbers = (int(num) for num in numbers)
 
 odd_numbers = (num for num in numbers if num % 2 == 1)
 even_numbers = (num for num in numbers if num % 2 == 0)
+
 # Writing the even number list to "even.txt"
+with open("even.txt", "w") as even_integers_file:
+    for num in even_numbers:
+        even_integers_file.write(str(num) + "\n")
+        
 # Writing the odd number list to "odd.txt"
 # Displaying that the program is executed
