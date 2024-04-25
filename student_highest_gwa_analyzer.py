450c81ba17411687bc038bc9579fe4d6fb32df66
Menard <menardsabugo@gmail.com>
2024-04-26 00:14:53 +0800

    Displaying that the program is executed

--- student_highest_gwa_analyzer.py
+++ student_highest_gwa_analyzer.py
@@ -6,5 +6,19 @@ with open("student_record.txt", "r") as students_data:
 highest_gwa = float("inf")
 highest_student_name = ""
 
-# Checking for the highest student's GWA
+# Finding for the highest student's GWA
+for line in lines:
+    name, gwa = line.strip().split(",")
+    gwa = float(gwa)
+
+    # Checking for the highest student's GWA
+    if gwa < highest_gwa:
+        highest_gwa = gwa
+        highest_student_name = name
+
 # Displaying the student who had the highest GWA
+intro = " Student With The Highest GWA "
+intro = intro.center(48, "-")
+print(intro)
+print(f"Name: {highest_student_name}")
+print(f"GWA: {highest_gwa}")
