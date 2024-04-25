47e92b3961bb856601087142666686b85a685ebc
Menard <menardsabugo@gmail.com>
2024-04-26 01:30:46 +0800

    Setting up a variable

--- text_writer_in_file.py
+++ text_writer_in_file.py
@@ -1,7 +1,8 @@
 # Open the file in append mode
 with open(mylife.txt, "a") as my_file:
 
-# Setting up a variable to hold the user's response.
+    # Setting up a variable to hold the user's response.
+    more_variables = "y"
 # Loop until the user confirms that there are no more lines
 # Collecting the user's input
 # Displaying the program's output
