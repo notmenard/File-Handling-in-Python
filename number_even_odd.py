3c8cfb4546be8e8624fd72201f05aeeb25ae99d0
Menard <menardsabugo@gmail.com>
2024-04-25 17:30:42 +0800

    Added some revisions

--- number_even_odd.py
+++ number_even_odd.py
@@ -3,7 +3,7 @@ with open("numbers.txt", "r") as new_file:
     numbers = new_file.read().split()
 
 # Separating the integers into even and odd number lists
-numbers = (int(num) for num in numbers)
+numbers = [int(num) for num in numbers]
 
 odd_numbers = (num for num in numbers if num % 2 == 1)
 even_numbers = (num for num in numbers if num % 2 == 0)
