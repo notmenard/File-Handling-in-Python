a0f6241567bdd14cc8df18c44fd39eaab806fe8d
Menard <menardsabugo@gmail.com>
2024-04-25 22:59:32 +0800

    Opening the student_record file

--- student_highest_gwa_analyzer.py
+++ student_highest_gwa_analyzer.py
@@ -1,4 +1,6 @@
 # Open the file containing student data and read all lines
+with open("student_record.txt", "r") as students_data:
+
 # Initialize variables to keep track of the student's highest GWA
 # Checking for the highest student's GWA
 # Displaying the student who had the highest GWA
