035c2a22a5650ee0f29550e8b63f23715714dec2
Menard <menardsabugo@gmail.com>
2024-04-25 16:43:56 +0800

    Separating the integers

--- number_even_odd.py
+++ number_even_odd.py
@@ -1,7 +1,13 @@
 # Reading the integers from "number.txt" file
 with open("numbers.txt", "r") as new_file:
     numbers = new_file.read().split()
+
 # Separating the integers into even and odd number lists
+
+numbers = (int(num) for num in numbers)
+
+odd_numbers = (num for num in numbers if num % 2 == 1)
+even_numbers = (num for num in numbers if num % 2 == 0)
 # Writing the even number list to "even.txt"
 # Writing the odd number list to "odd.txt"
 # Displaying that the program is executed
