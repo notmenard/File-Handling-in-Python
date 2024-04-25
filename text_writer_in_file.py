3ecff588a8ef87d868bad6f500c81d05ee0720ec
Menard <menardsabugo@gmail.com>
2024-04-26 01:49:22 +0800

    Displaying the program's output

--- text_writer_in_file.py
+++ text_writer_in_file.py
@@ -1,5 +1,5 @@
 # Open the file in append mode
-with open(mylife.txt, "a") as my_file:
+with open("mylife.txt", "a") as my_file:
 
     # Setting up a variable to hold the user's response.
     more_lines = "y"
@@ -8,4 +8,7 @@ with open(mylife.txt, "a") as my_file:
         # Collecting the user's input
         line = input("Enter line:")
         my_file.write(line + "\n")
-# Displaying the program's output
+        # Displaying the program's output
+        more_lines = input("Are there more lines (y/n)? ").lower()
+        if more_lines != "y":
+            break
