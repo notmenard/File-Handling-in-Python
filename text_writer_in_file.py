57bc67dbd5a9509c9f3238ae06be68d9e16f5b39
Menard <menardsabugo@gmail.com>
2024-04-26 01:26:18 +0800

    Pseudocode

---
+++ text_writer_in_file.py
@@ -0,0 +1,5 @@
+# Open the file in append mode
+# Setting up a variable to hold the user's response.
+# Loop until the user confirms that there are no more lines
+# Collecting the user's input
+# Displaying the program's output
