b1458cf13a09b6dd764471a3e0f38eb433c0a1eb
Menard <menardsabugo@gmail.com>
2024-04-25 23:16:03 +0800

    Reading all lines in the file

--- student_highest_gwa_analyzer.py
+++ student_highest_gwa_analyzer.py
@@ -1,5 +1,6 @@
 # Open the file containing student data and read all lines
 with open("student_record.txt", "r") as students_data:
+    lines = students_data.readlines()
 
 # Initialize variables to keep track of the student's highest GWA
 # Checking for the highest student's GWA
