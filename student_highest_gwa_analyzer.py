3dec0ead1ebbca3ccc69d94405a9d371fc450c3c
Menard <menardsabugo@gmail.com>
2024-04-25 23:21:15 +0800

    Initializing variables

--- student_highest_gwa_analyzer.py
+++ student_highest_gwa_analyzer.py
@@ -3,5 +3,8 @@ with open("student_record.txt", "r") as students_data:
     lines = students_data.readlines()
 
 # Initialize variables to keep track of the student's highest GWA
+highest_gwa = float("inf")
+highest_student_name = ""
+
 # Checking for the highest student's GWA
 # Displaying the student who had the highest GWA
