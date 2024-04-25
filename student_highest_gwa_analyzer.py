85cc1e124d16679cade07672bbb42caa439aef6d
Menard <menardsabugo@gmail.com>
2024-04-25 22:37:50 +0800

    Pseudocode

---
+++ student_highest_gwa_analyzer.py
@@ -0,0 +1,4 @@
+# Open the file containing student data and read all lines
+# Initialize variables to keep track of the student's highest GWA
+# Checking for the highest student's GWA
+# Displaying the student who had the highest GWA
