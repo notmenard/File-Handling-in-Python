afe57f73f03f4b928830fe4dd3e6cbf761b765c6
Menard <menardsabugo@gmail.com>
2024-04-26 14:19:13 +0800

    Reading the integers

--- sort_out_even_squared_odd_cubed.py
+++ sort_out_even_squared_odd_cubed.py
@@ -2,7 +2,8 @@
 with open("integers.txt", "r") as my_file:
     # Creating the two output files
     with open("double.txt", "w") as double_file, open("triple.txt", "w") as triple_file:
-# Reading the integers from the source file
+        # Reading the integers from the source file
+        for line in my_file:
 # Converting the line into an integer
 # Checking for the integer if even
 # Writing the square of the even number to the double.txt file
