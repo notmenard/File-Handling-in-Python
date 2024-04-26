cdc9b6d7c4dee3e9655ad246c9daf89f840dd398
Menard <menardsabugo@gmail.com>
2024-04-26 14:20:40 +0800

    Converting the line to integer

--- sort_out_even_squared_odd_cubed.py
+++ sort_out_even_squared_odd_cubed.py
@@ -4,7 +4,9 @@ with open("integers.txt", "r") as my_file:
     with open("double.txt", "w") as double_file, open("triple.txt", "w") as triple_file:
         # Reading the integers from the source file
         for line in my_file:
-# Converting the line into an integer
+            # Converting the line into an integer
+            number = int(line)
+
 # Checking for the integer if even
 # Writing the square of the even number to the double.txt file
 # Writing the cube of the odd number to triple.txt file
